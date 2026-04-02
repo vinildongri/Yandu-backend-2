afa29edd04775cf342cf89f31b5cf13c0f4d692d
Vinil Dongri <dongrivinil@gmail.com>
2026-04-02 12:43:53 +0530

    fixed logout logic

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -175,15 +175,16 @@ export const loginUser = catchAsyncErrors(async (req, res, next) => {
 
 // Logout User => /api/v1/logout 
 export const logout = catchAsyncErrors(async (req, res, next) => {
-    res.cookie("token", null, {
-        expires: new Date(Date.now()),
+    res.cookie("token", " ", {
+        expires: new Date(0),
         httpOnly: true,
         secure: process.env.NODE_ENV === "PRODUCTION",
         sameSite: process.env.NODE_ENV === "PRODUCTION" ? "none" : "lax",
+        path: "/",
     });
 
     res.status(200).json({
-        message: "Logged Out Successfully",
+        message: "Logged Out Successfully!",
     });
 });
 
